aaf067626af23ffcedc6f3b3681f554ad919e056
Attila Max Ruf <therufa@gmail.com>
2023-05-03 20:38:52 +0200

    config blaming

--- lua/custom/plugins/gitblame.lua
+++ lua/custom/plugins/gitblame.lua
@@ -1,4 +1,14 @@
 
+vim.cmd('let g:gitblame_enabled = 0')
+
+-- git blame
+vim.keymap.set('n', '<leader>Gb', function()
+	vim.cmd(':GitBlameEnable')
+end, { desc = '[G]it [b]lame enable' })
+vim.keymap.set('n', '<leader>Gd', function()
+	vim.cmd(':GitBlameDisable')
+end, { desc = '[G]it blame [D]isable' })
+
 return {
 	{
 		'f-person/git-blame.nvim',
--- lua/custom/plugins/treesj.lua
+++ lua/custom/plugins/treesj.lua
@@ -1,5 +1,4 @@
 
-
 return {
 	'Wansmer/treesj',
   keys = {
